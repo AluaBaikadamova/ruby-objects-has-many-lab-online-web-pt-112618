25694a750b1e2ce7eccf0633a2f58efb9dc65b51
Alua Baikadamova <abaikad00@gmail.com>
2019-01-15 07:09:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -13,8 +13,24 @@ class Post
   def self.all
     @@all
   end
-    def author_name
-    self.author.name
+  
+    def author=(author)
+    @author = author
   end
+   def author
+     @author
+   end
+  
+  def author_name
+ if self.author!=nil 
+   self.author.name
+ else
+   nil
+ end
+end
+  def self.find_by_name(name)
+    @@all.detect{|a| a.name == name}
+  end
+end
  
 end
